69293258aa0382b8874c98e501b6032b4cd0eca1
GoyalChirag <itschiraggoyal@gmail.com>
2020-04-28 16:00:06 +0530

    user registered

--- myweb/views.py
+++ myweb/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Destination
+from django.contrib.auth.models import User,auth
 
 
 def index(request):
@@ -7,4 +8,18 @@ def index(request):
     return render(request,'index.html',{'dests': dests})
 
 def register(request):
-    return render(request,'register.html')
+    if request.method=='POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        # birthday = request.POST['birthday']
+        email = request.POST['email']
+        # phone = request.POST['phone']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        user = User.objects.create_user(username = username, first_name = first_name, last_name = last_name, email = email, password = password1)
+        user.save();
+        print('user created')
+        return redirect('/')
+    else:
+        return render(request,'register.html')
