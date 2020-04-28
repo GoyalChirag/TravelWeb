5771db8747ed3f1fdcd0d5c8d726272743b5e58e
GoyalChirag <itschiraggoyal@gmail.com>
2020-04-28 13:55:05 +0530

    db done

--- myweb/views.py
+++ myweb/views.py
@@ -3,6 +3,5 @@ from .models import Destination
 
 
 def index(request):
-    dest1= Destination()
-    dest1.name = 'Mumbai'
-    return render(request,'index.html',{'dest1': dest1})
+    dests=Destination.objects.all()
+    return render(request,'index.html',{'dests': dests})
