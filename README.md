08738d2f530dc58a2b28300973b6df75fe77bab1
Chirag Goyal <itschirag@gmail.com>
2022-01-20 16:00:02 +0530

    New

--- README.md
+++
@@ -1,2 +0,0 @@
-# TravelWeb
-Project for learning Django and Postgresql
--- myweb/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Destination,Newsletter
-
-admin.site.register(Destination)
-admin.site.register(Newsletter)
